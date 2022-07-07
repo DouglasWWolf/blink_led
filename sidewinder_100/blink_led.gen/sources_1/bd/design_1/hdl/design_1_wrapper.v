cd7be8ce7f6d3071187b43a9eedb059bc28d2c79
DouglasWWolf <douglaswolf@yahoo.com>
2022-07-07 13:05:28 -0700

    No commit message

--- sidewinder_100/blink_led.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ sidewinder_100/blink_led.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
-//Date        : Thu Jun 16 12:53:20 2022
+//Date        : Thu Jul  7 12:46:01 2022
 //Host        : simtool5-2 running 64-bit Ubuntu 20.04.4 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
@@ -11,18 +11,18 @@
 
 module design_1_wrapper
    (GPIO_LED0,
-    sysclk100_n,
-    sysclk100_p);
+    clk_100mhz_clk_n,
+    clk_100mhz_clk_p);
   output GPIO_LED0;
-  input [0:0]sysclk100_n;
-  input [0:0]sysclk100_p;
+  input [0:0]clk_100mhz_clk_n;
+  input [0:0]clk_100mhz_clk_p;
 
   wire GPIO_LED0;
-  wire [0:0]sysclk100_n;
-  wire [0:0]sysclk100_p;
+  wire [0:0]clk_100mhz_clk_n;
+  wire [0:0]clk_100mhz_clk_p;
 
   design_1 design_1_i
        (.GPIO_LED0(GPIO_LED0),
-        .sysclk100_n(sysclk100_n),
-        .sysclk100_p(sysclk100_p));
+        .clk_100mhz_clk_n(clk_100mhz_clk_n),
+        .clk_100mhz_clk_p(clk_100mhz_clk_p));
 endmodule
--- sidewinder_100/xdc/board.xdc
+++ sidewinder_100/xdc/board.xdc
@@ -1,13 +1,13 @@
 
 # 100 MHZ differential clock
-set_property -dict {PACKAGE_PIN C4  IOSTANDARD LVDS_25} [ get_ports sysclk100_p ]
-set_property -dict {PACKAGE_PIN C3  IOSTANDARD LVDS_25} [ get_ports sysclk100_n ]
+set_property -dict {PACKAGE_PIN C4  IOSTANDARD LVDS_25} [ get_ports clk_100mhz_clk_p ]
+set_property -dict {PACKAGE_PIN C3  IOSTANDARD LVDS_25} [ get_ports clk_100mhz_clk_n ]
 
 # The LED
 set_property -dict {PACKAGE_PIN B5 IOSTANDARD LVCMOS33} [ get_ports GPIO_LED0 ]
 
 # Create the clock
-create_clock -period 10.000 -name clk_100mhz [get_ports sysclk100_p]
+create_clock -period 10.000 -name clock_100mhz [get_ports clk_100mhz_clk_p]
 
 # Compress the resulting bitstream
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
